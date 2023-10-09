7d57ef709bb193e78fbaad533b71264b99444cfc
Jerry May A. Alor <alorjerrymay@gmail.com>
2023-10-09 20:04:54 +0800

    Database Updated

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'customwpthmem_db' );
+define( 'DB_NAME', 'customwptheme_db' );
 
 /** Database username */
 define( 'DB_USER', 'root' );

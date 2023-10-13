0ff1d55d0b732362725936096df034eb60e1c480
Jerry May A. Alor <alorjerrymay@gmail.com>
2023-10-14 05:46:20 +0800

    first

---
+++ wp-content/themes/jerrytheme/index.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * Main template file 
+ * 
+ *  @package Jerry Theme
+ */
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>WordPress Custom Theme</title>
+</head>
+<body>
+     <h1>custom theme</h1>
+     
+</body>
+</html>
